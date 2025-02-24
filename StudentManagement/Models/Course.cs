a7cee945a37acd33985173726fd0ca22a509daa5
TheDarkSide1992 <Jenscp171@gmail.com>
2025-02-24 11:13:38 +0100

    init models and dataContext

---
+++ StudentManagement/Models/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentManagement.Models;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public float Credits { get; set; }
+}
---
+++ StudentManagement/databaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+
+namespace StudentManagement;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Enrollment> Enrollments { get; set; }
+    public DbSet<Student> Students { get; set; }
+    
+    public string DbPath { get; }
+
+    protected DatabaseContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "StudentManagement.db");
+        
+        Console.WriteLine(DbPath);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
