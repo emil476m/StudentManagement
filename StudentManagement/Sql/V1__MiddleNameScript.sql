290ebf79964d4f7c72468f9f96702c6c223a6d09
TheDarkSide1992 <Jenscp171@gmail.com>
2025-02-24 13:53:46 +0100

    added middlename to student

---
+++ StudentManagement/Sql/V1__MiddleNameScript.sql
@@ -0,0 +1,8 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE "Students" ADD "MiddleName" TEXT NOT NULL DEFAULT '';
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250224125254_V1__MiddleName', '9.0.2');
+
+COMMIT;
+
